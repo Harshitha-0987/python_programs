a4bf2cc35bbc09f23bf9765e86443dda274cb91a
Harshitha-0987 <nagaharshithadivi@gmail.com>
2024-07-13 19:24:40 +0530

    Create second_smallest_and_largest_element_in_a_lost.py

---
+++ second_smallest_and_largest_element_in_a_lost.py
@@ -0,0 +1,7 @@
+def find(list1):
+    length=len(list1)
+    list1.sort()
+    print('second larget is',list1[length-2])
+    print('second smallest is',list1[1])
+list1=[12,4,15,78,90,]
+large=find(list1)
